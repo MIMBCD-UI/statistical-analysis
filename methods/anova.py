28bce753dc31b7708da3bcb03d8b039e102fa252
FMCalisto <FMCalisto@users.noreply.github.com>
2018-06-19 17:23:58 +0100

    [ADD] Source Code

---
+++ methods/anova.py
@@ -0,0 +1,15 @@
+# import sys
+# sys.path.insert(0, '../../sheet-reader/src/')
+
+# from src import main
+
+# MIN_VAL = 4
+# MAX_VAL = 12
+
+# MIN_ROW = str(MIN_VAL)
+# MAX_ROW = str(MAX_VAL)
+
+# SCAPE_CHAR = ':'
+# MENTAL_DEMAND_ROW = 'C'
+
+# sheetReader(MENTAL_DEMAND_ROW, MIN_VAL, MAX_VAL)
