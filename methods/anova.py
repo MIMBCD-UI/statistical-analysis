4f8424a10332c8bf59c9401201c9e1e0a90df262
FMCalisto <FMCalisto@users.noreply.github.com>
2018-06-21 11:54:20 +0100

    [ADD] Links Between Sheet Reader and Statistical Analysis Repositories

--- methods/anova.py
+++ methods/anova.py
@@ -1,15 +1,16 @@
-# import sys
-# sys.path.insert(0, '../../sheet-reader/src/')
+import sys, os.path
+sheetReader_dir = (os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
++ '/sheet-reader/src/')
+sys.path.append(sheetReader_dir)
+from main import sheetReader
 
-# from src import main
+MIN_VAL = 4
+MAX_VAL = 12
 
-# MIN_VAL = 4
-# MAX_VAL = 12
+MIN_ROW = str(MIN_VAL)
+MAX_ROW = str(MAX_VAL)
 
-# MIN_ROW = str(MIN_VAL)
-# MAX_ROW = str(MAX_VAL)
+SCAPE_CHAR = ':'
+MENTAL_DEMAND_ROW = 'C'
 
-# SCAPE_CHAR = ':'
-# MENTAL_DEMAND_ROW = 'C'
-
-# sheetReader(MENTAL_DEMAND_ROW, MIN_VAL, MAX_VAL)
+sheetReader(MENTAL_DEMAND_ROW, MIN_VAL, MAX_VAL)
