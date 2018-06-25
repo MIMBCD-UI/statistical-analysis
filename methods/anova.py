cf627da64b83d7b02a10c4c9cdc8757da2e656dc
FMCalisto <FMCalisto@users.noreply.github.com>
2018-06-25 10:43:20 +0100

    [UPDATE] Sheet Reader Sum

--- methods/anova.py
+++ methods/anova.py
@@ -2,7 +2,9 @@ import sys, os.path
 sheetReader_dir = (os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 + '/sheet-reader/src/')
 sys.path.append(sheetReader_dir)
+
 from main import sheetReader
+from main import sheetReaderSum
 
 MIN_VAL = 4
 MAX_VAL = 12
@@ -13,4 +15,6 @@ MAX_ROW = str(MAX_VAL)
 SCAPE_CHAR = ':'
 MENTAL_DEMAND_ROW = 'C'
 
-sheetReader(MENTAL_DEMAND_ROW, MIN_VAL, MAX_VAL)
+#sheetReader(MENTAL_DEMAND_ROW, MIN_VAL, MAX_VAL)
+
+sheetReaderSum(MENTAL_DEMAND_ROW, MIN_VAL, MAX_VAL)
