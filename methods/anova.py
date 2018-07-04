fc19ed99d876aeb290fd8447db68c734eb422beb
FMCalisto <FMCalisto@users.noreply.github.com>
2018-07-04 19:11:22 +0100

    [CREATE] Normal Test Mechanism

--- methods/anova.py
+++ methods/anova.py
@@ -38,6 +38,8 @@ constants_dir = (pathAbsPath + '/sheet-reader/constants/')
 sys.path.append(constants_dir)
 import main_variables
 
+MV_N = main_variables.N
+
 scripts_dir = (pathAbsPath + '/sheet-reader/scripts/')
 sys.path.append(scripts_dir)
 import sheetReaders
@@ -51,6 +53,7 @@ from scipy import stats
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
+import numpy as np
 
 datafile_fs = pd.read_csv(fs_sheet_dir)
 datafile_fm = pd.read_csv(fm_sheet_dir)
@@ -192,6 +195,213 @@ d_data_fm_f = {
 k = len(pd.unique(datafile_fs.group))  # number of conditions
 N = len(datafile_fs.values)  # conditions times participants
 n = datafile_fs.groupby('group').size()[0] #Participants in each condition
+
+# ============================================== #
+#                                                #
+#      IS IT NORMAL (GAUSSIAN) DISTRIBUTION?     #
+#                                                #
+# ============================================== #
+
+s_001 = datafile_fs.mental_demand[:MV_N].tolist()
+s_002 = datafile_fs.physical_demand[:MV_N].tolist()
+s_003 = datafile_fs.temporal_demand[:MV_N].tolist()
+s_004 = datafile_fs.performance[:MV_N].tolist()
+s_005 = datafile_fs.effort[:MV_N].tolist()
+s_006 = datafile_fs.frustration[:MV_N].tolist()
+
+s_007 = datafile_fm.mental_demand[:MV_N].tolist()
+s_008 = datafile_fm.physical_demand[:MV_N].tolist()
+s_009 = datafile_fm.temporal_demand[:MV_N].tolist()
+s_010 = datafile_fm.performance[:MV_N].tolist()
+s_011 = datafile_fm.effort[:MV_N].tolist()
+s_012 = datafile_fm.frustration[:MV_N].tolist()
+
+print(s_001)
+print(s_002)
+print(s_003)
+print(s_004)
+print(s_005)
+print(s_006)
+
+print(s_007)
+print(s_008)
+print(s_009)
+print(s_010)
+print(s_011)
+print(s_012)
+
+# ============================================== #
+# Mean:
+mu_s_001 = np.mean(s_001, axis=0)
+mu_s_002 = np.mean(s_002, axis=0)
+mu_s_003 = np.mean(s_003, axis=0)
+mu_s_004 = np.mean(s_004, axis=0)
+mu_s_005 = np.mean(s_005, axis=0)
+mu_s_006 = np.mean(s_006, axis=0)
+
+mu_s_007 = np.mean(s_007, axis=0)
+mu_s_008 = np.mean(s_008, axis=0)
+mu_s_009 = np.mean(s_009, axis=0)
+mu_s_010 = np.mean(s_010, axis=0)
+mu_s_011 = np.mean(s_011, axis=0)
+mu_s_012 = np.mean(s_012, axis=0)
+# ============================================== #
+
+# ============================================== #
+# Standard Deviation:
+sigma = 0.1
+stdToCompare = 0.01
+# ============================================== #
+
+# ============================================== #
+# Compute Standard Deviation:
+stdComp_s_001 = np.mean(s_001)
+stdComp_s_002 = np.mean(s_002)
+stdComp_s_003 = np.mean(s_003)
+stdComp_s_004 = np.mean(s_004)
+stdComp_s_005 = np.mean(s_005)
+stdComp_s_006 = np.mean(s_006)
+print("Compute Standard Deviation for stdComp_s_001: %d" % stdComp_s_001)
+print("Compute Standard Deviation for stdComp_s_002: %d" % stdComp_s_002)
+print("Compute Standard Deviation for stdComp_s_003: %d" % stdComp_s_003)
+print("Compute Standard Deviation for stdComp_s_004: %d" % stdComp_s_004)
+print("Compute Standard Deviation for stdComp_s_005: %d" % stdComp_s_005)
+print("Compute Standard Deviation for stdComp_s_006: %d" % stdComp_s_006)
+
+stdComp_s_007 = np.mean(s_007)
+stdComp_s_008 = np.mean(s_008)
+stdComp_s_009 = np.mean(s_009)
+stdComp_s_010 = np.mean(s_010)
+stdComp_s_011 = np.mean(s_011)
+stdComp_s_012 = np.mean(s_012)
+print("Compute Standard Deviation for stdComp_s_007: %d" % stdComp_s_007)
+print("Compute Standard Deviation for stdComp_s_008: %d" % stdComp_s_008)
+print("Compute Standard Deviation for stdComp_s_009: %d" % stdComp_s_009)
+print("Compute Standard Deviation for stdComp_s_010: %d" % stdComp_s_010)
+print("Compute Standard Deviation for stdComp_s_011: %d" % stdComp_s_011)
+print("Compute Standard Deviation for stdComp_s_012: %d" % stdComp_s_012)
+# ============================================== #
+
+# ============================================== #
+# Verification of the Mean:
+mean_ver_s_001 = abs(mu_s_001 - stdComp_s_001) < stdToCompare
+mean_ver_s_002 = abs(mu_s_002 - stdComp_s_002) < stdToCompare
+mean_ver_s_003 = abs(mu_s_003 - stdComp_s_003) < stdToCompare
+mean_ver_s_004 = abs(mu_s_004 - stdComp_s_004) < stdToCompare
+mean_ver_s_005 = abs(mu_s_005 - stdComp_s_005) < stdToCompare
+mean_ver_s_006 = abs(mu_s_006 - stdComp_s_006) < stdToCompare
+print("Verification of the Mean for mean_ver_s_001: %r" % mean_ver_s_001)
+print("Verification of the Mean for mean_ver_s_002: %r" % mean_ver_s_002)
+print("Verification of the Mean for mean_ver_s_003: %r" % mean_ver_s_003)
+print("Verification of the Mean for mean_ver_s_004: %r" % mean_ver_s_004)
+print("Verification of the Mean for mean_ver_s_005: %r" % mean_ver_s_005)
+print("Verification of the Mean for mean_ver_s_006: %r" % mean_ver_s_006)
+
+mean_ver_s_007 = abs(mu_s_007 - stdComp_s_007) < stdToCompare
+mean_ver_s_008 = abs(mu_s_008 - stdComp_s_008) < stdToCompare
+mean_ver_s_009 = abs(mu_s_009 - stdComp_s_009) < stdToCompare
+mean_ver_s_010 = abs(mu_s_010 - stdComp_s_010) < stdToCompare
+mean_ver_s_011 = abs(mu_s_011 - stdComp_s_011) < stdToCompare
+mean_ver_s_012 = abs(mu_s_012 - stdComp_s_012) < stdToCompare
+print("Verification of the Mean for mean_ver_s_007: %r" % mean_ver_s_007)
+print("Verification of the Mean for mean_ver_s_008: %r" % mean_ver_s_008)
+print("Verification of the Mean for mean_ver_s_009: %r" % mean_ver_s_009)
+print("Verification of the Mean for mean_ver_s_010: %r" % mean_ver_s_010)
+print("Verification of the Mean for mean_ver_s_011: %r" % mean_ver_s_011)
+print("Verification of the Mean for mean_ver_s_012: %r" % mean_ver_s_012)
+# ============================================== #
+
+# ============================================== #
+# Compute Variance:
+stdS_001 = np.std(s_001, ddof=1)
+stdS_002 = np.std(s_002, ddof=1)
+stdS_003 = np.std(s_003, ddof=1)
+stdS_004 = np.std(s_004, ddof=1)
+stdS_005 = np.std(s_005, ddof=1)
+stdS_006 = np.std(s_006, ddof=1)
+print("Compute Variance for stdS_001: %f" % stdS_001)
+print("Compute Variance for stdS_002: %f" % stdS_002)
+print("Compute Variance for stdS_003: %f" % stdS_003)
+print("Compute Variance for stdS_004: %f" % stdS_004)
+print("Compute Variance for stdS_005: %f" % stdS_005)
+print("Compute Variance for stdS_006: %f" % stdS_006)
+
+stdS_007 = np.std(s_007, ddof=1)
+stdS_008 = np.std(s_008, ddof=1)
+stdS_009 = np.std(s_009, ddof=1)
+stdS_010 = np.std(s_010, ddof=1)
+stdS_011 = np.std(s_011, ddof=1)
+stdS_012 = np.std(s_012, ddof=1)
+print("Compute Variance for stdS_007: %f" % stdS_007)
+print("Compute Variance for stdS_008: %f" % stdS_008)
+print("Compute Variance for stdS_009: %f" % stdS_009)
+print("Compute Variance for stdS_010: %f" % stdS_010)
+print("Compute Variance for stdS_011: %f" % stdS_011)
+print("Compute Variance for stdS_012: %f" % stdS_012)
+# ============================================== #
+
+# ============================================== #
+# Verification of the Variance:
+variance_ver_s_001 = abs(sigma - stdS_001) < stdToCompare
+variance_ver_s_002 = abs(sigma - stdS_002) < stdToCompare
+variance_ver_s_003 = abs(sigma - stdS_003) < stdToCompare
+variance_ver_s_004 = abs(sigma - stdS_004) < stdToCompare
+variance_ver_s_005 = abs(sigma - stdS_005) < stdToCompare
+variance_ver_s_006 = abs(sigma - stdS_006) < stdToCompare
+print("Verification of the Variance for variance_ver_s_001: %r" % variance_ver_s_001)
+print("Verification of the Variance for variance_ver_s_002: %r" % variance_ver_s_002)
+print("Verification of the Variance for variance_ver_s_003: %r" % variance_ver_s_003)
+print("Verification of the Variance for variance_ver_s_004: %r" % variance_ver_s_004)
+print("Verification of the Variance for variance_ver_s_005: %r" % variance_ver_s_005)
+print("Verification of the Variance for variance_ver_s_006: %r" % variance_ver_s_006)
+
+variance_ver_s_007 = abs(sigma - stdS_007) < stdToCompare
+variance_ver_s_008 = abs(sigma - stdS_008) < stdToCompare
+variance_ver_s_009 = abs(sigma - stdS_009) < stdToCompare
+variance_ver_s_010 = abs(sigma - stdS_010) < stdToCompare
+variance_ver_s_011 = abs(sigma - stdS_011) < stdToCompare
+variance_ver_s_012 = abs(sigma - stdS_012) < stdToCompare
+print("Verification of the Variance for variance_ver_s_007: %r" % variance_ver_s_007)
+print("Verification of the Variance for variance_ver_s_008: %r" % variance_ver_s_008)
+print("Verification of the Variance for variance_ver_s_009: %r" % variance_ver_s_009)
+print("Verification of the Variance for variance_ver_s_010: %r" % variance_ver_s_010)
+print("Verification of the Variance for variance_ver_s_011: %r" % variance_ver_s_011)
+print("Verification of the Variance for variance_ver_s_012: %r" % variance_ver_s_012)
+# ============================================== #
+
+# ============================================== #
+# Normal Validation:
+normalValidation_s_001 = stats.normaltest(s_001)
+normalValidation_s_002 = stats.normaltest(s_002)
+normalValidation_s_003 = stats.normaltest(s_003)
+normalValidation_s_004 = stats.normaltest(s_004)
+normalValidation_s_005 = stats.normaltest(s_005)
+normalValidation_s_006 = stats.normaltest(s_006)
+print(normalValidation_s_001)
+print(normalValidation_s_002)
+print(normalValidation_s_003)
+print(normalValidation_s_004)
+print(normalValidation_s_005)
+print(normalValidation_s_006)
+
+normalValidation_s_001 = stats.normaltest(s_007)
+normalValidation_s_001 = stats.normaltest(s_008)
+normalValidation_s_001 = stats.normaltest(s_009)
+normalValidation_s_001 = stats.normaltest(s_010)
+normalValidation_s_001 = stats.normaltest(s_011)
+normalValidation_s_001 = stats.normaltest(s_012)
+print(normalValidation_s_007)
+print(normalValidation_s_008)
+print(normalValidation_s_009)
+print(normalValidation_s_010)
+print(normalValidation_s_011)
+print(normalValidation_s_012)
+# ============================================== #
+
+# ============================================== #
+# ============================================== #
+# ============================================== #
+# ============================================== #
 
 # ============================================== #
 #                                                #
@@ -402,13 +612,6 @@ print("Multi-Modality (Frustration): p = %f" % (p_fm_f))
 
 # ============================================== #
 # ============================================== #
-
-
-
-
-
-
-
 
 # ============================================== #
 # ============================================== #
