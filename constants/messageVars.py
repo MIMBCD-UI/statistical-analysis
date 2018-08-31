ce61c2de6e39c253520011a00fc997fba7fac7d2
FMCalisto <FMCalisto@users.noreply.github.com>
2018-08-31 17:58:11 +0100

    [CREATE] Message Vars

---
+++ constants/messageVars.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+""".py: """
+
+__author__      = "Francisco Maria Calisto"
+__maintainer__  = "Francisco Maria Calisto"
+__email__       = "francisco.calisto@tecnico.ulisboa.pt"
+__license__     = "ACADEMIC & COMMERCIAL"
+__version__     = "1.0.0"
+__status__      = "Production"
+__copyright__   = "Copyright 2017, Instituto Superior Técnico (IST)"
+__credits__     = [
+  "Carlos Santiago",
+  "Jacinto C. Nascimento",
+  "Pedro Miraldo",
+  "Nuno Nunes"
+]
+
+message_001 = "Compute Standard Deviation for MD (SM):" + "\n"
+message_002 = "Compute Standard Deviation for PD (SM):" + "\n"
+message_003 = "Compute Standard Deviation for TD (SM):" + "\n"
+message_004 = "Compute Standard Deviation for PE (SM):" + "\n"
+message_005 = "Compute Standard Deviation for EF (SM):" + "\n"
+message_006 = "Compute Standard Deviation for FR (SM):" + "\n"
+
+message_007 = "Compute Standard Deviation for MD (MM):" + "\n"
+message_008 = "Compute Standard Deviation for PD (MM):" + "\n"
+message_009 = "Compute Standard Deviation for TD (MM):" + "\n"
+message_010 = "Compute Standard Deviation for PE (MM):" + "\n"
+message_011 = "Compute Standard Deviation for EF (MM):" + "\n"
+message_012 = "Compute Standard Deviation for FR (MM):" + "\n"
+
+message_013 = "Compute Standard Deviation for SUS01 (SM):" + "\n"
+message_014 = "Compute Standard Deviation for SUS02 (SM):" + "\n"
+message_015 = "Compute Standard Deviation for SUS03 (SM):" + "\n"
+message_016 = "Compute Standard Deviation for SUS04 (SM):" + "\n"
+message_017 = "Compute Standard Deviation for SUS05 (SM):" + "\n"
+message_018 = "Compute Standard Deviation for SUS06 (SM):" + "\n"
+message_019 = "Compute Standard Deviation for SUS07 (SM):" + "\n"
+message_020 = "Compute Standard Deviation for SUS08 (SM):" + "\n"
+message_021 = "Compute Standard Deviation for SUS09 (SM):" + "\n"
+message_022 = "Compute Standard Deviation for SUS10 (SM):" + "\n"
+
+message_023 = "Compute Standard Deviation for SUS01 (MM):" + "\n"
+message_024 = "Compute Standard Deviation for SUS02 (MM):" + "\n"
+message_025 = "Compute Standard Deviation for SUS03 (MM):" + "\n"
+message_026 = "Compute Standard Deviation for SUS04 (MM):" + "\n"
+message_027 = "Compute Standard Deviation for SUS05 (MM):" + "\n"
+message_028 = "Compute Standard Deviation for SUS06 (MM):" + "\n"
+message_029 = "Compute Standard Deviation for SUS07 (MM):" + "\n"
+message_030 = "Compute Standard Deviation for SUS08 (MM):" + "\n"
+message_031 = "Compute Standard Deviation for SUS09 (MM):" + "\n"
+message_032 = "Compute Standard Deviation for SUS10 (MM):" + "\n"
+
+message_033 = "Compute Standard Deviation for TIME__94662 (SM):" + "\n"
+message_034 = "Compute Standard Deviation for TIME_607376 (SM):" + "\n"
+message_035 = "Compute Standard Deviation for TIME_737037 (SM):" + "\n"
+message_036 = "Compute Standard Deviation for TIME__TOTAL (SM):" + "\n"
+
+message_037 = "Compute Standard Deviation for CLICKS__94662 (SM):" + "\n"
+message_038 = "Compute Standard Deviation for CLICKS_607376 (SM):" + "\n"
+message_039 = "Compute Standard Deviation for CLICKS_737037 (SM):" + "\n"
+message_040 = "Compute Standard Deviation for CLICKS__TOTAL (SM):" + "\n"
+
+message_041 = "Compute Standard Deviation for ERRORS__94662 (SM):" + "\n"
+message_042 = "Compute Standard Deviation for ERRORS_607376 (SM):" + "\n"
+message_043 = "Compute Standard Deviation for ERRORS_737037 (SM):" + "\n"
+message_044 = "Compute Standard Deviation for ERRORS__TOTAL (SM):" + "\n"
+
+message_045 = "Compute Standard Deviation for BIRADS__94662 (SM):" + "\n"
+message_046 = "Compute Standard Deviation for BIRADS_607376 (SM):" + "\n"
+message_047 = "Compute Standard Deviation for BIRADS_737037 (SM):" + "\n"
+
+message_048 = "Compute Standard Deviation for TIME__94662 (MM):" + "\n"
+message_049 = "Compute Standard Deviation for TIME_607376 (MM):" + "\n"
+message_050 = "Compute Standard Deviation for TIME_737037 (MM):" + "\n"
+message_051 = "Compute Standard Deviation for TIME__TOTAL (MM):" + "\n"
+
+message_052 = "Compute Standard Deviation for CLICKS__94662 (MM):" + "\n"
+message_053 = "Compute Standard Deviation for CLICKS_607376 (MM):" + "\n"
+message_054 = "Compute Standard Deviation for CLICKS_737037 (MM):" + "\n"
+message_055 = "Compute Standard Deviation for CLICKS__TOTAL (MM):" + "\n"
+
+message_056 = "Compute Standard Deviation for ERRORS__94662 (MM):" + "\n"
+message_057 = "Compute Standard Deviation for ERRORS_607376 (MM):" + "\n"
+message_058 = "Compute Standard Deviation for ERRORS_737037 (MM):" + "\n"
+message_059 = "Compute Standard Deviation for ERRORS__TOTAL (MM):" + "\n"
+
+message_060 = "Compute Standard Deviation for BIRADS__94662 (MM):" + "\n"
+message_061 = "Compute Standard Deviation for BIRADS_607376 (MM):" + "\n"
+message_062 = "Compute Standard Deviation for BIRADS_737037 (MM):" + "\n"
