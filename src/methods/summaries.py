7e4b407ff74244811ac9168ad95c58a45f814d2c
Francisco Maria Calisto <francisco_mcalisto@outlook.com>
2019-07-26 11:52:35 +0100

    [NEW] Summary Count: NASA-TLX & BIRADS Intro

--- src/methods/summaries.py
+++ src/methods/summaries.py
@@ -26,11 +26,78 @@ saJoinPath = os.path.join(pathAbsPath, 'statistical-analysis')
 sys.path.append(saJoinPath)
 saAbsPath = os.path.abspath(saJoinPath)
 
-saConsJoinPath = os.path.join(saAbsPath, 'constants')
-sys.path.append(saConsJoinPath)
-saConsAbsPath = os.path.abspath(saConsJoinPath)
+saSrcJoinPath = os.path.join(saAbsPath, 'src')
+sys.path.append(saSrcJoinPath)
+saSrcAbsPath = os.path.abspath(saSrcJoinPath)
+
+saSrcConsJoinPath = os.path.join(saSrcAbsPath, 'constants')
+sys.path.append(saSrcConsJoinPath)
+saSrcConsAbsPath = os.path.abspath(saSrcConsJoinPath)
+
+import pandas as pd
+import scipy.stats as stats
+import researchpy as rp
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+    
+import matplotlib.pyplot as plt
 
 from sheets import *
-df.drop('person', axis= 1, inplace= True)
+
+# ============================================== #
+#                                                #
+#                       UTA7                     #
+#                                                #
+# ============================================== #
+
+# birads_assis.csv
+# birads_crrnt.csv
+# birads_phy.csv
+# birads_real.csv
+# dots_assis.csv
+# nasatlx_assis.csv
+# nasatlx_crrnt.csv
+# noc_assis.csv
+# noc_crrnt.csv
+# noe_ce_assis.csv
+# noe_ce_crrnt.csv
+# noe_nce_assis.csv
+# noe_nce_crrnt.csv
+# sus_assis.csv
+# sus_crrnt.csv
+# time_assis_avtr.csv
+# time_assis_dgns.csv
+# time_ext_all.csv
+# time_ext_reg.csv
+# time_full_assis.csv
+# time_full_crrnt.csv
+
+# ============================================== #
+#           UTA7: Current vs Assistant           #
+# ============================================== #
+
+# NASA-TLX
+
+rp_nasatlx_crrnt_mendem = rp.summary_cont(df_nasatlx_crrnt['mental_demand'])
+print (rp_nasatlx_crrnt_mendem)
+
+rp_nasatlx_assis_mendem = rp.summary_cont(df_nasatlx_assis['mental_demand'])
+print (rp_nasatlx_assis_mendem)
+
+# BIRADS
+
+rp_birads_assis_low = rp.summary_cont(df_birads_assis['low'])
+print (rp_birads_assis_low)
+
+rp_birads_phy_low = rp.summary_cont(df_birads_phy['low'])
+print (rp_birads_phy_low)
+
+# ============================================== #
+# ============================================== #
+
+# ============================================== #
+# ============================================== #
+# ============================================== #
+# ============================================== #
 
 # ==================== END File ==================== #
